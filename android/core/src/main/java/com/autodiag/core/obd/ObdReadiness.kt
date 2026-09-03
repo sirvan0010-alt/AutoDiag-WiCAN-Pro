bc3e07633aeac0f708885fcd06c7bdeec305c527
sirvan0010-alt <sirvan0010@gmail.com>
2026-09-03 23:35:18 +0200

    fix(obd): readiness decoder uses C/D bit masks correctly

--- android/core/src/main/java/com/autodiag/core/obd/ObdReadiness.kt
+++ android/core/src/main/java/com/autodiag/core/obd/ObdReadiness.kt
@@ -26,37 +26,41 @@ object ObdReadinessDecoder {
         val count = a and 0x7F
         val spark = (b and 0x08) == 0
         val monitors = mutableListOf<MonitorStatus>()
-        fun bit(v: Int, supportedBit: Int, readyBit: Int, id: String, name: String) {
-            val supported = (v and supportedBit) != 0
-            val incomplete = (v and readyBit) != 0
+        fun add(v: Int, supportMask: Int, readyMask: Int, id: String, name: String) {
+            val supported = (v and supportMask) != 0
+            val incomplete = (v and readyMask) != 0
             monitors += MonitorStatus(id, name, supported, if (supported) !incomplete else null)
         }
-        bit(b, 0x01, 0x10, "misfire", "Výpadky zapalování")
-        bit(b, 0x02, 0x20, "fuel", "Palivový systém")
-        bit(b, 0x04, 0x40, "ccm", "Komplexní součásti")
+        add(b, 0x01, 0x10, "misfire", "Výpadky zapalování")
+        add(b, 0x02, 0x20, "fuel", "Palivový systém")
+        add(b, 0x04, 0x40, "ccm", "Komplexní součásti")
         if (spark) {
-            bit(c, 0x01, d and 0x01, "cat", "Katalyzátor")
-            bit(c, 0x02, d and 0x02, "hcat", "Vyhřívaný katalyzátor")
-            bit(c, 0x04, d and 0x04, "evap", "EVAP")
-            bit(c, 0x08, d and 0x08, "sair", "Sekundární vzduch")
-            bit(c, 0x10, d and 0x10, "ac", "A/C chladivo")
-            bit(c, 0x20, d and 0x20, "o2", "Kyslíkový senzor")
-            bit(c, 0x40, d and 0x40, "o2h", "Ohřev O2")
-            bit(c, 0x80, d and 0x80, "egr", "EGR/VVT")
+            fun cd(mask: Int, id: String, name: String) {
+                val supported = (c and mask) != 0
+                val incomplete = (d and mask) != 0
+                monitors += MonitorStatus(id, name, supported, if (supported) !incomplete else null)
+            }
+            cd(0x01, "cat", "Katalyzátor")
+            cd(0x02, "hcat", "Vyhřívaný katalyzátor")
+            cd(0x04, "evap", "EVAP")
+            cd(0x08, "sair", "Sekundární vzduch")
+            cd(0x10, "ac", "A/C chladivo")
+            cd(0x20, "o2", "Kyslíkový senzor")
+            cd(0x40, "o2h", "Ohřev O2")
+            cd(0x80, "egr", "EGR/VVT")
         } else {
-            bit(c, 0x01, d and 0x01, "nmhc", "NMHC katalyzátor")
-            bit(c, 0x02, d and 0x02, "nox", "NOx/SCR")
-            bit(c, 0x04, d and 0x04, "boost", "Boost pressure")
-            bit(c, 0x08, d and 0x08, "egs", "Výfukový senzor")
-            bit(c, 0x20, d and 0x20, "pf", "Filtr pevných částic")
-            bit(c, 0x80, d and 0x80, "egr", "EGR/VVT")
+            fun cd(mask: Int, id: String, name: String) {
+                val supported = (c and mask) != 0
+                val incomplete = (d and mask) != 0
+                monitors += MonitorStatus(id, name, supported, if (supported) !incomplete else null)
+            }
+            cd(0x01, "nmhc", "NMHC katalyzátor")
+            cd(0x02, "nox", "NOx/SCR")
+            cd(0x04, "boost", "Boost pressure")
+            cd(0x08, "egs", "Výfukový senzor")
+            cd(0x20, "pf", "Filtr pevných částic")
+            cd(0x80, "egr", "EGR/VVT")
         }
         return ReadinessReport(mil, count, spark, monitors)
-    }
-
-    private fun bit(c: Int, supportedMask: Int, readyMask: Int, id: String, name: String): MonitorStatus {
-        val supported = (c and supportedMask) != 0
-        val incomplete = (readyMask) != 0
-        return MonitorStatus(id, name, supported, if (supported) !incomplete else null)
     }
 }
