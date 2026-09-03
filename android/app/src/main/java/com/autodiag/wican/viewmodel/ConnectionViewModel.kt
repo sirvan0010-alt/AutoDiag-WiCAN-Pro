b27c3ee95a1501c261e30b4998643b5e0e4abd9a
sirvan0010-alt <sirvan0010@gmail.com>
2026-09-03 12:27:45 +0200

    Fix metrics flow import in connection ViewModel

--- android/app/src/main/java/com/autodiag/wican/viewmodel/ConnectionViewModel.kt
+++ android/app/src/main/java/com/autodiag/wican/viewmodel/ConnectionViewModel.kt
@@ -17,6 +17,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
