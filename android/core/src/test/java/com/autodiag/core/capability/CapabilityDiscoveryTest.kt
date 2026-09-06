ba9c3a017a948e722cd2c8704e66a5620c92ce00
sirvan0010-alt <sirvan0010@gmail.com>
2026-09-06 21:20:30 +0200

    Restore single Mode 01 bitmap regression test

---
+++ android/core/src/test/java/com/autodiag/core/capability/CapabilityDiscoveryTest.kt
@@ -0,0 +1,17 @@
+package com.autodiag.core.capability
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class CapabilityDiscoveryTest {
+    @Test
+    fun supportedMode01BitmapMapsMsbFirstToFirstPid() {
+        val bytes = intArrayOf(0x80, 0x00, 0x00, 0x01)
+        val supported = (0 until 32).filter { bit ->
+            val byteIndex = bit / 8
+            val bitInByte = 7 - (bit % 8)
+            (bytes[byteIndex] and (1 shl bitInByte)) != 0
+        }.map { it + 1 }
+        assertEquals(listOf(1, 32), supported)
+    }
+}
