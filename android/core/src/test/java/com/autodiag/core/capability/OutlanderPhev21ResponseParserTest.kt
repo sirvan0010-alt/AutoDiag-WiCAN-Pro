5a330d4000ee6e6e097ab7e7b350b68ff14916a5
sirvan0010-alt <sirvan0010@gmail.com>
2026-09-10 07:38:52 +0200

    test(isotp): enforce declared payload length cases

--- android/core/src/test/java/com/autodiag/core/capability/OutlanderPhev21ResponseParserTest.kt
+++ android/core/src/test/java/com/autodiag/core/capability/OutlanderPhev21ResponseParserTest.kt
@@ -8,7 +8,7 @@ class OutlanderPhev21ResponseParserTest {
     @Test
     fun stripsCanHeaderIsoTpAndPositiveResponsePrefix() {
         val response = """
-            762 10 37 61 01 82 83 0F 8B
+            762 10 0D 61 01 82 83 0F 8B
             762 21 24 0F 88 03 0C 6E 52
         """.trimIndent()
 
@@ -18,21 +18,20 @@ class OutlanderPhev21ResponseParserTest {
     }
 
     @Test
-    fun acceptsMultilineIsoTpPayload() {
+    fun acceptsMultilineIsoTpPayloadWhenDeclaredLengthMatches() {
         val response = """
             7E8 10 05 61 01 AA BB CC
-            7E8 21 DD
         """.trimIndent()
 
         val parsed = OutlanderPhev21ResponseParser.parse(response)
 
-        assertEquals(listOf(0xAA, 0xBB, 0xCC, 0xDD), parsed.toList())
+        assertEquals(listOf(0xAA, 0xBB, 0xCC), parsed.toList())
     }
 
     @Test
     fun preservesThirtyTwo21_04OutputPositionsAfterTransportAndServiceHeaders() {
         val response = """
-            762 10 24 61 04 BE BF C0 C1
+            762 10 22 61 04 BE BF C0 C1
             762 21 C2 C3 C4 C5 C6 C7 C8
             762 22 C9 CA CB CC CD CE CF
             762 23 D0 D1 D2 D3 D4 D5 D6
@@ -52,5 +51,20 @@ class OutlanderPhev21ResponseParserTest {
         assertFailsWith<IllegalArgumentException> {
             OutlanderPhev21ResponseParser.parse("762 10 37 61")
         }
+    }
+
+    @Test
+    fun rejectsTruncatedConsecutiveFrameSequence() {
+        val response = """
+            762 10 37 61 01 00 01 02 03
+            762 21 04 05 06 07 08 09 0A
+        """.trimIndent()
+
+        val error = assertFailsWith<OutlanderPhev21ResponseParser.IsoTpLengthMismatchException> {
+            OutlanderPhev21ResponseParser.parse(response)
+        }
+
+        assertEquals(53, error.declaredPayloadSize)
+        assertEquals(10, error.actualPayloadSize)
     }
 }
